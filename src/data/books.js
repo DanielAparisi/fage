937139ea2a1f5e22402a0994d26ac2081a61e0fd
Daniel Aparisi <daniel.aparisi.lozano.com>
2023-12-14 22:27:34 +0100

    init commit

---
+++ src/data/books.js
@@ -0,0 +1,23 @@
+export const books = [
+    {
+        id: 'Almacenaje',
+        title: 'Proceso de almacenaje',
+        img:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Mercadona_em_Canidelo.jpg/330px-Mercadona_em_Canidelo.jpg'
+    },
+    {
+        id: 'Almacen',
+        title: 'Proceso de Almacenamiento',
+        img:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Mercadona_em_Canidelo.jpg/330px-Mercadona_em_Canidelo.jpg'
+    },
+    {
+        id: 'Clasificacion',
+        title: 'Proceso de Clasificacion de los productos',
+        img:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Mercadona_em_Canidelo.jpg/330px-Mercadona_em_Canidelo.jpg'
+    },
+    {
+        id: 'Control',
+        title: 'Proceso de Control de los productos',
+        img:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Mercadona_em_Canidelo.jpg/330px-Mercadona_em_Canidelo.jpg'
+    },
+
+]
